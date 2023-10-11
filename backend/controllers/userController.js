ce6c250528626bf5d33135f41f1d21e21379345f
Sanchit Poudel <snchtpdl12@gmail.com>
2023-10-11 14:32:32 +0545

    feat: :zap: Update Profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -36,11 +36,11 @@ const signupUser = async (req, res) => {
 
 const loginUser = async (req, res) => {
     try {
-		const { username, password } = req.body;
-		const user = await User.findOne({ username });
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
         const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
 
-		if (!user || !isPasswordCorrect) return res.status(400).json({ message: "Invalid username or password" });
+        if (!user || !isPasswordCorrect) return res.status(400).json({ message: "Invalid username or password" });
         generateTokenAndCookie(user._id, res);
         res.status(200).json({
             _id: user._id,
@@ -55,5 +55,88 @@ const loginUser = async (req, res) => {
 
     }
 }
+const logoutUser = (req, res) => {
+    try {
+        res.cookie("jwt", "", { maxAge: 1 });
+        res.status(200).json({ message: "User logged out successfully" });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+        console.log("Error in signupUser: ", err.message);
+    }
+};
+const followUnfollowUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const userToModify = await User.findById(id);
+        const currentUser = await User.findById(req.user._id);
+        if (id === req.user._id.toString()) return res.status(400).json({ error: "You cannot follow/Unfollow yourself" });
 
-export { signupUser, loginUser };
+        if (!userToModify || !currentUser) return res.status(400).json({ error: "User not found" });
+        const isFollowing = currentUser.following.includes(id);
+        if (isFollowing) {
+            // unfollow user
+
+            await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+            await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
+            res.status(200).json({ message: "User Unfollowed successfully" })
+
+
+        }
+        else {
+            await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } })
+            await User.findByIdAndUpdate(req.user._id, { $push: { following: id } })
+            res.status(200).json({ message: "User followed successfully " })
+
+
+
+        }
+
+
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+        console.log("Error in Follow/Unfollow user: ", err.message);
+    }
+}
+const updateUser = async (req, res) => {
+
+    const { name, email, username, password, bio } = req.body;
+    let { profilePic } = req.body;
+    const userId = req.user._id;
+    try {
+        let user = await User.findById(userId);
+        if (!user) return res.status(400).json({ error: "User not found" });
+        if (req.params.id !== userId.toString())
+            return res.status(400).json({ error: "You cannot update other user's profile" });
+        if (password) {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            user.password = hashedPassword;
+        }
+        if (profilePic) {
+            if (user.profilePic) {
+                await cloudinary.uploader.destroy(user.profilePic.split("/").pop().split(".")[0]);
+            }
+
+            const uploadedResponse = await cloudinary.uploader.upload(profilePic);
+            profilePic = uploadedResponse.secure_url;
+        }
+        user.name = name || user.name;
+        user.email = email || user.email;
+        user.username = username || user.username;
+        user.profilePic = profilePic || user.profilePic;
+        user.bio = bio || user.bio;
+		user = await user.save();
+        res.status(200).json({ message: "Profile updated successfully", user })
+
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+        console.log("Error in Follow/Unfollow user: ", err.message);
+
+    }
+}
+
+
+
+export { signupUser, loginUser, logoutUser, followUnfollowUser, updateUser };
